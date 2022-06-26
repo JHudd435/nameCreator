1f8e7e85b1bc9ba6b038653ee5ba7aea047231b7
JHudd435 <97488098+JHudd435@users.noreply.github.com>
2022-06-26 14:32:22 -0500

    Delete src/nameGen directory

--- src/nameGen/F/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-
-Generate a female first name
-
-"""
-
-__version__ = "0.1.0"
-__author__ = 'Justin Huddleston'
--- src/nameGen/L/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Generate a last name
-
-"""
-
-__version__ = "0.1.0"
-__author__ = 'Justin Huddleston'
