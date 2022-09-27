f57e82f024a1d250a91a598f44149b82c8e31842
JHudd15 <justinhuddleston073@gmail.com>
2022-09-27 09:48:36 -0500

    Added output flag CLI.

--- nameCreatorCLI.py
+++ nameCreatorCLI.py
@@ -1,5 +1,6 @@
 import nameCreator
 import click
+import io
 
 
 @click.group()
@@ -14,19 +15,29 @@ def nameCreatorCLI():
               '--language',
               help='Language/Nationality of the generated name(s).')
 @click.option('-a', '--amount', help='How many names are generated.')
+@click.option(
+    '-o',
+    '--output',
+    help="Where to save the generation. If left empty, does not save.")
 @nameCreatorCLI.command()
-def generate(gender: str, language: str, amount=str):
+def generate(gender: str, language: str, amount=str, output=str):
     """Generates names. Supported languages are american, russian, spanish, italian, german, french, finnish, and swedish."""
     if (gender.lower() == "male" or gender.lower() == "m"):
         for i in range(int(amount)):
             firstname = nameCreator.firstM.FirstNameMale(language)
             lastname = nameCreator.last.LastName(language)
             print(firstname + " " + lastname)
+            if (output != None):
+                with io.open(output, 'a', encoding='utf-8') as f:
+                    f.write(firstname + " " + lastname + "\n")
     elif (gender.lower() == "female" or gender.lower() == "f"):
         for i in range(int(amount)):
             firstname = nameCreator.firstF.FirstNameFemale(language)
             lastname = nameCreator.last.LastName(language)
             print(firstname + " " + lastname)
+            if (output != None):
+                with io.open(output, 'a', encoding='utf-8') as f:
+                    f.write(firstname + " " + lastname + "\n")
     else:
         print(gender.lower)
         print("Please enter male or female as gender.")
